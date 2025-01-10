813b6d4d8c005f2f438b5aa2631c77e2316548c1
Marcel Kedziora <nuage.clan@gmail.com>
2025-01-10 13:06:18 +0000

    debugging, importmap.rb new test

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,7 +6,7 @@ class ListsController < ApplicationController
   def show
     @list = List.find(params[:id])
   end
-  
+
   def new
     @list = List.new
   end
@@ -14,10 +14,16 @@ class ListsController < ApplicationController
   def create
     @list = List.new(list_params)
     if @list.save
-      redirect_to list_path(@list), notice: 'List was successfully created.'
+      redirect_to list_path(@list)
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to lists_path
   end
 
   private
